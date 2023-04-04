ac738830af5b4a00d0501931d48b5f2dfd2ed7ea
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-04 14:36:20 +0800

    Update 4108056041_05_LSBMR.py

--- hw5/4108056041_05_LSBMR.py
+++ hw5/4108056041_05_LSBMR.py
@@ -1,13 +1,28 @@
 import cv2
 import numpy as np
 import os
+import random
+
+RATIO = 0.2
+
+
+def lsbmr(secret_message):
+    x = 9
+
 
 if __name__ == "__main__":
+
+    # initial variables
     dir_awc1 = "test"
-
-    entries = os.listdir(dir_awc1)
-
     list = []
+
+    # create secret message
+    random.seed(1000)
+    secret_message = bin(random.randint(0, 1000))
+    print(secret_message)
+
+    # read pic in test file
+    entries = os.listdir(dir_awc1)
 
     for entry in entries:
         img_tar = cv2.imread(dir_awc1 + "/" + entry)
@@ -15,5 +30,6 @@ if __name__ == "__main__":
 
         list.append(img_tar)
 
+    # embedding message with LSBMR
     for i in range(9):
         print(i+1)
