bfdfc38495d49da85f67e96cd3e029d1f861b0de
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-29 22:40:50 +0800

    Update 4108056041_07_2D_EAT_enc.py

--- hw7/4108056041_07_2D_EAT_enc.py
+++ hw7/4108056041_07_2D_EAT_enc.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
 import os
-import random
-import math
 
 a, b = 1, 1
 M = np.array([[1, a],
@@ -10,27 +8,49 @@ M = np.array([[1, a],
 G = 100
 
 
-def enc(sou_img):
+def cal_coordinate(sou_img):
+    h, w, c = sou_img.shape
+    pixels = h
 
-    h, w, c = list[i].shape
-    pixels = h * w
-    print(pixels)
+    list1 = []
 
-    transfer = []
+    for i in range(h):
+        list2 = []
 
-    haha = [1, 1]
-    hehe = np.dot(M, haha)
-    hehe[0] = hehe[0] % pixels
-    hehe[1] = hehe[1] % pixels
+        for j in range(w):
+            list2.append([i, j])
 
-    print(hehe)
+        list1.append(list2)
 
-    # for i in range(G):
-    #     for j in range(h):
-    #         for k in range(w):
-    #             x = 0
+    # print(list1)
 
-    transfer = transfer.reshape(h, w)
+    this_img = list1
+    new_img = list1
+
+    for i in range(G):
+        for j in range(h):
+            for k in range(w):
+                tmp_coordinate = [j, k]
+
+                new_coordinate = np.dot(M, tmp_coordinate)
+                new_coordinate[0] = new_coordinate[0] % pixels
+                new_coordinate[1] = new_coordinate[1] % pixels
+
+                new_img[new_coordinate[0]][new_coordinate[1]] = this_img[j][k]
+
+        this_img = new_img
+
+    return this_img
+
+
+def enc(sou_img, transfer):
+    res_img = sou_img
+
+    for j in range(512):
+        for k in range(512):
+            res_img[j][k] = sou_img[transfer[j][k][0]][transfer[j][k][1]]
+
+    return res_img
 
 
 if __name__ == "__main__":
@@ -52,15 +72,17 @@ if __name__ == "__main__":
         list.append(img)
         list_name.append(entry.replace(".png", ""))
 
+    transfer = cal_coordinate(list[0])
+
     # EAT enc
     for i in range(9):
 
         print(i)
         print(list_name[i])
         h, w, c = list[i].shape
-        #pixels = h * w
-        # print(pixels)
-        enc(list[i])
 
-    print("\n")
-    print(M)
+        result_img = enc(list[i], transfer)
+
+        # create enc img
+        path_name = dir_encryp + list_name[i] + "_enc.png"
+        cv2.imwrite(path_name, result_img)
