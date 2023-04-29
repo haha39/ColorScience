cc96e17b010044c1ca7b70c7bbdd0b3e788c6fc3
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-30 03:02:42 +0800

    Update 4108056041_07_2D_EAT_enc.py

--- hw7/4108056041_07_2D_EAT_enc.py
+++ hw7/4108056041_07_2D_EAT_enc.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 a, b = 1, 1
-M = np.array([[1, a],
-              [b, a*b+1]])
+M = np.array([[a*b+1, a],
+              [b, 1]])
 G = 100
 
 
@@ -12,20 +12,25 @@ def cal_coordinate(sou_img):
     h, w, c = sou_img.shape
     pixels = h
 
-    list1 = []
+    this_img = []
 
     for i in range(h):
-        list2 = []
+        list1 = []
 
         for j in range(w):
-            list2.append([i, j])
+            list1.append([i, j])
 
-        list1.append(list2)
+        this_img.append(list1)
 
-    # print(list1)
+    new_img = []
 
-    this_img = list1
-    new_img = list1
+    for i in range(h):
+        list1 = []
+
+        for j in range(w):
+            list1.append([i, j])
+
+        new_img.append(list1)
 
     for i in range(G):
         for j in range(h):
@@ -38,7 +43,11 @@ def cal_coordinate(sou_img):
 
                 new_img[new_coordinate[0]][new_coordinate[1]] = this_img[j][k]
 
-        this_img = new_img
+    for a in range(2):
+        for b in range(2):
+            this_img[a][b] = new_img[a][b]
+
+    # print(this_img)
 
     return this_img
 
@@ -49,6 +58,8 @@ def enc(sou_img, transfer):
     for j in range(512):
         for k in range(512):
             res_img[j][k] = sou_img[transfer[j][k][0]][transfer[j][k][1]]
+            # print(i)
+            # print(transfer[j][k][0])
 
     return res_img
 
@@ -79,7 +90,6 @@ if __name__ == "__main__":
 
         print(i)
         print(list_name[i])
-        h, w, c = list[i].shape
 
         result_img = enc(list[i], transfer)
 
