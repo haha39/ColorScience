b38ae03805e19b66f187a4a2be1318e542941689
haha39 <60254357+haha39@users.noreply.github.com>
2023-05-25 04:15:57 +0800

    Update 3_COR_measure.py

--- hw9/3_COR_measure.py
+++ hw9/3_COR_measure.py
@@ -1,37 +1,94 @@
 import cv2
 import numpy as np
 import os
+import csv
+import math
+
+# from scipy.stats import chi2_contingency
+# import matplotlib.pyplot as plt
 
 
-def voh(img):
+def cor(img):
 
-    var_BGR = []
+    size = 512*512
 
-    for color in range(3):
+    list1 = list(range(0, size))
 
-        # build histgram
-        hist_img = cv2.calcHist(
-            [img], [color], None, [256], [0, 256])
-        cv2.normalize(hist_img, hist_img, alpha=0,
-                      beta=1, norm_type=cv2.NORM_MINMAX)
+    list_choice = np.random.choice(a=list1, size=8000, replace=False)
 
-        # calculate the var of histogram
-        var = 0
+    # find four coorelation : x, y_hor, y_ver, y_dia
+    xi = []
+    y_hor = []
+    y_ver = []
+    y_dia = []
 
-        for i in range(256):
-            for j in range(256):
-                var = ((hist_img[i] - hist_img[j]) ** 2) / 2
+    for i in range(8000):
 
-        var = var / 256
-        var = var / 256
+        x = math.floor(list_choice[0] / 512)
+        y = list_choice[i] % 512
 
-        var_BGR.append(var)
+        xi.append(img[x][y])
 
-    return var_BGR
+        # y horizontal
+        if y != 511:
+            y_hor.append(img[x][y+1])
+        else:
+            y_hor.append(img[x][y-1])
+        # y vertical
+        if x != 511:
+            y_ver.append(img[x+1][y])
+        else:
+            y_ver.append(img[x-1][y])
+        # y diagonal
+        if x != 511 and y != 511:
+            y_dia.append(img[x+1][y+1])
+        else:
+            y_dia.append(img[x-1][y-1])
+
+    # calculate pearson's coefficients
+    # print(len(xi))
+    # print(len(y_hor))
+    # print(len(y_ver))
+    # print(len(y_dia))
+
+    # step 1
+    x_mean = np.mean(xi)
+    y_hor_mean = np.mean(y_hor)
+    y_ver_mean = np.mean(y_ver)
+    y_dia_mean = np.mean(y_dia)
+
+    cov_hor, cov_ver, cov_dia = 0.0, 0.0, 0.0
+    var_x_hor, var_x_ver, var_x_dia = 0.0, 0.0, 0.0
+    var_y_hor, var_y_ver, var_y_dia = 0.0, 0.0, 0.0
+
+    for i in range(8000):
+
+        # horizontal
+        cov_hor += (xi[i] - x_mean)*(y_hor[i] - y_hor_mean)
+        var_x_hor += ((xi[i] - x_mean) ** 2)
+        var_y_hor += ((y_hor[i] - y_hor_mean) ** 2)
+
+        # vertical
+        cov_ver += (xi[i] - x_mean)*(y_ver[i] - y_ver_mean)
+        var_x_ver += ((xi[i] - x_mean) ** 2)
+        var_y_ver += ((y_ver[i] - y_ver_mean) ** 2)
+
+        # diagonal
+        cov_dia += (xi[i] - x_mean)*(y_dia[i] - y_dia_mean)
+        var_x_dia += ((xi[i] - x_mean) ** 2)
+        var_y_dia += ((y_dia[i] - y_dia_mean) ** 2)
+
+    # step 2
+    r_hor = cov_hor / ((math.sqrt(var_x_hor))*(math.sqrt(var_y_hor)))
+    r_ver = cov_ver / ((math.sqrt(var_x_ver))*(math.sqrt(var_y_ver)))
+    r_dia = cov_dia / ((math.sqrt(var_x_dia))*(math.sqrt(var_y_dia)))
+
+    return (r_hor, r_ver, r_dia)
 
 
 if __name__ == "__main__":
 
+    # read pic in source file
     dir_sou = "source"
     list_sou = []
     list_sou_name = []
@@ -41,41 +98,56 @@ if __name__ == "__main__":
 
     for entry in entries:
 
-        img = cv2.imread(dir_sou + "/" + entry)
+        img = cv2.imread(dir_sou + "/" + entry, cv2.IMREAD_GRAYSCALE)
 
         # get image
         list_sou.append(img)
         # get name
         list_sou_name.append(entry.replace(".png", ""))
 
-    dir_encryp = "encryp"
-    list_encryp = []
-    list_encryp_name = []
+    # read pic in encryp file
+    dir_enc = "encryp"
+    list_enc = []
+    list_enc_name = []
 
-    # read pic in source file
-    entries = os.listdir(dir_encryp)
+    entries = os.listdir(dir_enc)
 
     for entry in entries:
 
-        img = cv2.imread(dir_encryp + "/" + entry)
+        img = cv2.imread(dir_enc + "/" + entry, cv2.IMREAD_GRAYSCALE)
 
         # get image
-        list_encryp.append(img)
+        list_enc.append(img)
         # get name
-        list_encryp_name.append(entry.replace(".png", ""))
+        list_enc_name.append(entry.replace(".png", ""))
 
-    # calculate variance of histogram
+    # calculate chi-square test of histogram
+    res_sou = []
+    res_enc = []
+
     for i in range(9):
 
         print(i)
         print(list_sou_name[i])
-        print(list_encryp_name)
+        print(list_enc_name[i])
 
-        result_sou = voh(list_sou[i])
-        result_enc = voh(list_encryp[i])
+        res_sou.append(cor(list_sou[i]))
+        res_enc.append(cor(list_enc[i]))
 
-        csv要的是rgb喔
+        # print(cor(list_sou[i]))
+        # print(cor(list_enc[i]))
 
-        # # create enc img
-        # path_name = dir_encryp + list_name[i] + "_enc.png"
-        # cv2.imwrite(path_name, result_img)
+    # create csv file
+    path = "statis/CHI_res.csv"
+
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["CHI", "", "Plain", "", "", "", "", "Cipher"])
+        writer.writerow(["Image", "Type", "Red", "Green",
+                        "Blue", "alpha", "chi value", "Red", "Green", "Blue"])
+'''
+        for i in range(9):
+            writer.writerow(
+                [list_sou_name[i], "gray", "", "", "",
+                 "0.05", "293.248", result[i]])
+'''
